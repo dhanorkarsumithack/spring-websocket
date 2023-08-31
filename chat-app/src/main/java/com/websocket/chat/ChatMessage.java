0ef2ff90cc143d120a3327b5e2c6ee220d961fab
sumitd <sumit.dhanorkar@neml.in>
2023-08-31 13:15:47 +0530

    chat app with websocket example

---
+++ chat-app/src/main/java/com/websocket/chat/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.websocket.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+
+    private String sender;
+
+    private  MessageType type;
+}
